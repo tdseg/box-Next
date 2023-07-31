3c6d9980ce9321d980bad9c389a184272a0ab753
tdseg <96424025+tdseg@users.noreply.github.com>
2023-07-31 06:34:51 +0000

    Update start.sh

--- start.sh
+++ start.sh
@@ -7,18 +7,18 @@ ParameterSSENCYPT=chacha20-ietf-poly1305
 CADDYIndexPage=https://showip.net/
 XRayConfig=https://raw.githubusercontent.com/bsefwe/codesandbox/main/etc/Caddyfile
 Con=https://raw.githubusercontent.com/bsefwe/codesandbox/main/etc/config.json
-# download execution
-wget "https://caddyserver.com/api/download?os=linux&arch=amd64" -O caddy
-wget "https://github.com/XTLS/Xray-core/releases/latest/download/Xray-linux-64.zip" -O xray-linux-64.zip
-unzip -o xray-linux-64.zip && rm -rf xray-linux-64.zip
-mv xray web
-chmod +x caddy web
+
 
 # set caddy
 mkdir -p etc/caddy/ usr/share/caddy
 echo -e "User-agent: *\nDisallow: /" > usr/share/caddy/robots.txt
 wget $CADDYIndexPage -O usr/share/caddy/index.html && unzip -qo usr/share/caddy/index.html -d usr/share/caddy/ && mv usr/share/caddy/*/* usr/share/caddy/
 
+# download execution
+wget "https://github.com/XTLS/Xray-core/releases/latest/download/Xray-linux-64.zip" -O xray-linux-64.zip
+unzip -o xray-linux-64.zip && rm -rf xray-linux-64.zip
+mv xray web
+chmod +x caddy web
 
 # set config file
 wget -qO- $XRayConfig | sed -e "1c :$PORT" -e "s/\$AUUID/$AUUID/g" -e "s/\$MYUUID-HASH/$(./caddy hash-password --plaintext $AUUID)/g" > etc/caddy/Caddyfile
